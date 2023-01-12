ce59d07130a99adb9d9c386f6942769e9f5067f0
Sectonic <sujaldhakal06@gmail.com>
2023-01-11 18:31:55 -0700

    all links changed

--- components/change_section.js
+++ components/change_section.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 function ChangeSection(props) {
   var learn_pages = [
     "start",
@@ -27,9 +29,9 @@ function ChangeSection(props) {
     return (
       <div className="change_section hidden-menu back_menu">
         <div className="change_back hidden-seen">
-          <a href="/typing">
+          <Link href="/typing">
             <img src="/img/main/section_btn.png" alt="" />
-          </a>
+          </Link>
         </div>
         <div className="change_forward hidden">
           <img src="/img/main/section_btn.png" alt="" />
@@ -41,22 +43,22 @@ function ChangeSection(props) {
       return (
         <div className="change_section">
           <div className="change_back">
-            <a href={`/learn/${learn_pages[index - 1]}`}>
+            <Link href={`/learn/${learn_pages[index - 1]}`}>
               <img src="/img/main/section_btn.png" alt="" />
-            </a>
+            </Link>
           </div>
           <div className="current">
-            <a href={`/learn/${learn_pages[index]}`}>
+            <Link href={`/learn/${learn_pages[index]}`}>
               <img
                 src={`/img/learn/home/${learn_pages[index]}.png`}
                 alt=""
               />
-            </a>
+            </Link>
           </div>
           <div className="change_forward">
-            <a href={`/learn/${learn_pages[index + 1]}`}>
+            <Link href={`/learn/${learn_pages[index + 1]}`}>
               <img src="/img/main/section_btn.png" alt="" />
-            </a>
+            </Link>
           </div>
         </div>
       );
@@ -71,24 +73,24 @@ function ChangeSection(props) {
           <div
             className={`change_back ${start_end ? "hidden" : "hidden-seen"}`}
           >
-            <a href={`/learn/${learn_pages[index - 1]}`}>
+            <Link href={`/learn/${learn_pages[index - 1]}`}>
               <img src="/img/main/section_btn.png" alt="" />
-            </a>
+            </Link>
           </div>
           <div className={`current ${start_end ? "hidden-current-left" : "hidden-current-right"}`}>
-            <a href={`/learn/${learn_pages[index]}`}>
+            <Link href={`/learn/${learn_pages[index]}`}>
               <img
                 src={`/img/learn/home/${learn_pages[index]}.png`}
                 alt=""
               />
-            </a>
+            </Link>
           </div>
           <div
             className={`change_forward ${start_end ? "hidden-seen hidden-seen-right" : "hidden"}`}
           >
-            <a href={`/learn/${learn_pages[index + 1]}`}>
+            <Link href={`/learn/${learn_pages[index + 1]}`}>
               <img src="/img/main/section_btn.png" alt="" />
-            </a>
+            </Link>
           </div>
         </div>
       );
