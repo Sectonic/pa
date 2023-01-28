0839c974f6b5cfe8a4b8efe7256db28601492d73
Aze7 <shadozvr@gmail.com>
2023-01-28 18:16:05 +0100

    added academy apps and polished other stuff

---
+++ pages/academyplus.js
@@ -0,0 +1,17 @@
+import Banner from "../components/banner";
+import Link from 'next/link';
+
+function AcademyPlus() {
+  return (
+    <div className="main">
+      <Banner
+        background="yellow"
+        title="Academy Plus"
+        section="support"
+        type="nav"
+      />
+    </div>
+  );
+}
+
+export default AcademyPlus;
