8ef1a569450104475ae3a02299e8b7fe3699fe87
Sectonic <sujaldhakal06@gmail.com>
2023-03-06 19:02:23 -0700

    added connection timeout

--- components/getsession.js
+++ components/getsession.js
@@ -4,11 +4,12 @@ import RedisStore from "connect-redis";
 import Redis from "ioredis";
 
 const production = process.env.NEXT_PUBLIC_API == "https://personalityacademyapi.pythonanywhere.com";
-const redisClient = !production ? new Redis() : new Redis({
+const redisClient = !production ? new Redis({connectTimeout: 10000}) : new Redis({
     port: 11218,
     host: "redis-11218.c232.us-east-1-2.ec2.cloud.redislabs.com",
     password: "t55wWsGk7PHhNTZK49ZogwaC5WzsAM1m",
-    username: "default"
+    username: "default",
+    connectTimeout: 10000
 })
 
 export const getSession = nextSession({
