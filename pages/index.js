04d5187b384edfe1d4e0cbdb2cbba042fffdd0c0
Aze <117712476+Aze7@users.noreply.github.com>
2023-04-22 21:22:53 +0200

    Revert "Update index.js"
    This reverts commit 1934f4f45fadf7fe88885283effead2299e1e10f.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,6 @@
 import { TypeAnimation } from 'react-type-animation';
 import ChangeSection from '../components/change_section';
 import {useRouter} from 'next/router';
-import Link from 'next/link';
-import { Main } from 'next/document';
-import { Popup } from '../components/popup_main';
 import  { Popup } from '../components/popup_main';
 import { useState } from 'react';
 
@@ -20,10 +17,6 @@ function Home() {
     setPopupType(type);
     setPopup(choice);
   };
-  const academyPlus = () => router.push('/academyplus');
-  return (
-    <div className="main">
-       {popupShown && <Popup popup={handlePopup} type={popupType} />}
 
   const academyPlus = () => router.push('/academyplus');
   return (
@@ -34,12 +27,12 @@ function Home() {
           <h1 className="home_title">
             <div>
               <h1 className="home_title_text">
-                <div className="home_title_top">Personality Academy</div>
-                <div >Objective Personality Made
+                <div className="home_title_top">Objective Personality</div>
+                <div >Made
                 <TypeAnimation
-                  sequence={['Simple.', 2250, 'Doable.', 2250]}
+                  sequence={['Simple.', 2250, 'Useful.', 2250, 'Doable.', 2250]}
                   repeat={Infinity}
-                  style={{ marginLeft: '10px'}}
+                  style={{ marginLeft: '20px'}}
                   wrapper="span"
                   className="home_subtitle"
                   speed={20} 
@@ -67,12 +60,10 @@ function Home() {
                 Support With
               </div>
               <div className="home_lg-textbig">
-                Academy Plus
+                Academy+
               </div>
             </div>
           </div>
-          <div className="home_dashboard-lg" 
-          onClick={() => handlePopup(true, "updates")}>
           <div className="home_dashboard-lg" onClick={() => handlePopup(true, "updates")} >
             <img src="/img/main/launch.png" className="updates_img" />
             <div className="home_lg-text">
@@ -80,7 +71,7 @@ function Home() {
               June 1st
               </div>
               <div className="home_lg-textbig">
-                Project Launch!
+                Project Launched!
               </div>
             </div>
           </div>
@@ -95,7 +86,10 @@ function Home() {
             />
           </div>
         </div>
-        <h2 className="section_title neg-mt-45">Everything You'll Need</h2>
+        <h2 className="section_title neg-mt-45">Everything OPS, In One Place</h2>
+        <p className="letters_subtitle">
+        *We are not affiliated with Objective Personality
+        </p>
         <div className="section_body section_texts-sm extra_spacing_texts-sm">
           <div className="section_text outline-trans">
             <div className="text-sm_img home-top_img">
@@ -104,9 +98,9 @@ function Home() {
               />
             </div>
             <div className="text-sm_desc">
-              <h3 className="section_subtitle margin-sm">Learn Personality</h3>
+              <h3 className="section_subtitle margin-sm">Learn</h3>
               <p>
-                Unfold the objectively trackable spectrum of personality.
+                Learn to objectively track personality traits through the OPS theory.
               </p>
             </div>
           </div>
@@ -117,9 +111,9 @@ function Home() {
               />
             </div>
             <div className="text-sm_desc">
-              <h3 className="section_subtitle margin-sm">Type Objectively</h3>
+              <h3 className="section_subtitle margin-sm">Type</h3>
               <p>
-              Use proper methods to track personality type objectively.
+              Use OPS methods to type yourself and others accurately.
               </p>
             </div>
           </div>
@@ -132,9 +126,9 @@ function Home() {
               />
             </div>
             <div className="text-sm_desc">
-              <h3 className="section_subtitle margin-sm">The Best Tools</h3>
+              <h3 className="section_subtitle margin-sm">Tools</h3>
               <p>
-                We'll help you along the way with the best easy to use tools.
+                We'll help you along the way with the best easy to use OPS tools.
               </p>
             </div>
           </div>
@@ -145,9 +139,9 @@ function Home() {
               />
             </div>
             <div className="text-sm_desc">
-              <h3 className="section_subtitle margin-sm">Actually Improve</h3>
+              <h3 className="section_subtitle margin-sm">Improve</h3>
               <p>
-              Turn knowledge into action by taking theory into practice.
+              Turn knowledge into action by taking theory into practise.
               </p>
             </div>
           </div>
@@ -157,55 +151,13 @@ function Home() {
       <div className="section_body">
       <div className="section_img">
             <img
-              src={"/img/main/science.png"}
-            />
-          </div>
-        </div>
-        <h2 className="section_title neg-mt-45">Objective Personality Theory</h2>
-        <p className="letters_subtitle">
-        *We are not affiliated with OPS, all credit goes to the original creators.
-        </p>
-        <div className="section_body section_texts-sm extra_spacing_texts-sm">
-          <div className="section_text outline-trans">
-            <div className="text-sm_img home-top_img">
-              <img
-                src={"/img/main/target.png"}
-              />
-            </div>
-            <div className="text-sm_desc">
-              <h3 className="section_subtitle margin-sm">Objective Trackability</h3>
-              <p>
-                An objective spectrum of personality that actually makes sense.
-              </p>
-            </div>
-          </div>
-          <div className="section_text outline-trans">
-            <div className="text-sm_img home-top_img">
-              <img
-                src={"/img/main/earth.png"}
-              />
-            </div>
-            <div className="text-sm_desc">
-              <h3 className="section_subtitle margin-sm">Down to Earth</h3>
-              <p>
-              Tracks things we see and hear every day, it's really nothing new.
-              </p>
-            </div>
-          </div>
-        </div>
-        <ChangeSection link="/resources" text="Official Source" src="tools" />
-      </div>
-      <div className="section extra_margin_section">
-      <div className="section_body">
-      <div className="section_img">
-            <img
-              src={"/img/academyplus/one.png"}
+              src={"/img/main/community.png"}
             />
           </div>
         </div>
         <h2 className="section_title neg-mt-45">All The Best, For The Community</h2>
         <p className="letters_subtitle">
-        A community project lead by Aze
+        Paving the way for better understanding, use and results for everyone.
         </p>
         <div className="section_body section_texts-sm extra_spacing_texts-sm">
           <div className="section_text outline-trans">
@@ -217,7 +169,7 @@ function Home() {
             <div className="text-sm_desc">
               <h3 className="section_subtitle margin-sm">Net Positive Impact</h3>
               <p>
-                Pushing OPS forward by pioneering the best platform for knowledge.
+                We focus on how to use OPS for good, to leave a real positive impact.
               </p>
             </div>
           </div>
@@ -236,55 +188,11 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="section typing_section extra_margin_section">
-        <h2 className="section_title">Get Started</h2>
-        <div className="section_body top_typing">
-          <div className="section_text outline-gray">
-            <Link href="/typing/triangulation">
-              <div>
-                <img
-                  src="/img/main/look.png"
-                  alt=""
-                  className="typing_img-home"
-                />
-                <h3 className="typing_title-h3">Take a Peek</h3>
-                <p>
-                Read about Personality Theory
-                </p>
-              </div>
-            </Link>
-          </div>
-          <div className="section_text outline-gray">
-            <Link href="/typing/d&s_typing">
-              <div>
-                <img
-                  src="/img/main/user.png"
-                  alt=""
-                  className="typing_img-home"
-                />
-                <h3 className="typing_title-h3">Sign Up</h3>
-                <p>
-                  Access for free with an account
-                </p>
-              </div>
-            </Link>
-          </div>
-        </div>
-      </div>
       <div className="section extra_margin_section">
-      <div className="section_body">
-      <div className="section_img">
-            <img
-              src={"/img/main/science.png"}
-            />
-          </div>
-        </div>
-        <h2 className="section_title neg-mt-45">Free Stuff With an Account</h2>
-        <ChangeSection link="/resources" text="Make an Account" src="tools" />
+        <h2 className="section_title">New To All This?</h2>
+        <ChangeSection link="/learn/start" text="Start Here" src="start" />
       </div>
     </div>
-    </div>
-  </div>
   );
 }
 
