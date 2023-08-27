fb4cc03057c3a80641adc8ad9daaea571fdba69e
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-08-26 22:08:54 -0600

    popup bg change

--- app/_components/popup.js
+++ app/_components/popup.js
@@ -2,7 +2,6 @@ function Popup({ children , onClose }) {
   return (
     <div className="popup_bg animate__animated animate__fadeIn">
       <div className="popup_main animate__animated animate__fade_in">
-        <div className="popup_bottom"></div>
         { onClose && (
           <div className="popup_exit" onClick={onClose}>
             <img
