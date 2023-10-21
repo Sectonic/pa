168accc9a3bf259b2b0d9de9914a00cb628243db
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-10-20 20:27:46 -0600

    crypt try catch

--- app/api/course_activity/route.js
+++ app/api/course_activity/route.js
@@ -8,22 +8,24 @@ export async function GET(request) {
     const session = request.nextUrl.searchParams.get('session');
     const course = request.nextUrl.searchParams.get('course');
 
-    const decrypted = crypt.decrypt(session, key);
-
-    if (!decrypted) {
-        return NextResponse.json([], { status: 404 })
-    }
-
-    if (!course || !session) {
-        return NextResponse.json([], { status: 404 })
-    }
-    
-    const pages = await db.courseActivity.findMany({
-        where: {
-            user: { id: Number(decrypted.toString(enc.Utf8)) },
-            url: { startsWith: '/' + course }
+    try {
+        const decrypted = crypt.decrypt(session, key);
+        
+        if (!course) {
+            return NextResponse.json([], { status: 404 })
         }
-    });
+        
+        const pages = await db.courseActivity.findMany({
+            where: {
+                user: { id: Number(decrypted.toString(enc.Utf8)) },
+                url: { startsWith: '/' + course }
+            }
+        });
+    
+        return NextResponse.json(pages, { status: 200 });
+    }
+    catch {
+        return NextResponse.json([], { status: 404 })
+    }
 
-    return NextResponse.json(pages, { status: 200 });
 }
