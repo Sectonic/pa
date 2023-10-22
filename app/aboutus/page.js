4785108a0e5fd86211b3979175dd7331c572acda
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-10-21 21:20:00 -0600

    font size changes to titles

--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <div className="main">
             <Banner
-                background="yellow"
+                background="blue"
                 title="About Us"
                 section="about"
                 type="nav"
