51697ae716f2d88ec6be17c30c44a74f614eb3e1
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-10-31 00:54:47 -0600

    metadata opengraph

--- app/_lib/metadata.js
+++ app/_lib/metadata.js
@@ -2,15 +2,15 @@
 export const createMetaData = ({ title, description, url, image}) => {
     const data = {
         metadataBase: new URL(process.env.PRODUCTION === 'true' ? 'https://personalityacademy.vercel.app' : 'http://localhost:3000'),
-        title: `${title ? title + ' | ' : ''}Personality Academy`,
-        description: description || 'Personality Theory Made Simple. Net Positive Impact.',
-        url: url || "/",
-        twitter: {
-            card: 'summary_large_image',
+        openGraph: {
             title: `${title ? title + ' | ' : ''}Personality Academy`,
             description: description || 'Personality Theory Made Simple. Net Positive Impact.',
             url: url || "/",
-            images: [`${image || '/embed/home.png'}`]
+            images: [{
+                url: image || '/embed/home.png',
+                width: 1291,
+                height: 685
+            }]
         }
     }
     return data;
