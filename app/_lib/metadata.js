5c160d46ca213bdb166b3d9d6f9c9ccd98d0082d
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-10-31 00:57:59 -0600

    changed image url oG

--- app/_lib/metadata.js
+++ app/_lib/metadata.js
@@ -2,12 +2,15 @@
 export const createMetaData = ({ title, description, url, image}) => {
     const data = {
         metadataBase: new URL(process.env.PRODUCTION === 'true' ? 'https://personalityacademy.vercel.app' : 'http://localhost:3000'),
+        title: `${title ? title + ' | ' : ''}Personality Academy`,
+        description: description || 'Personality Theory Made Simple. Net Positive Impact.',
+        url: url || "/",
         openGraph: {
             title: `${title ? title + ' | ' : ''}Personality Academy`,
             description: description || 'Personality Theory Made Simple. Net Positive Impact.',
             url: url || "/",
             images: [{
-                url: image || '/embed/home.png',
+                url: `/img/${image}` || '/embed/home.png',
                 width: 1291,
                 height: 685
             }]
