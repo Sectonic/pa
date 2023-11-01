6cfc55c989af3c60aea8228dd72700797af28508
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-11-01 01:09:08 -0600

    testing login metadata

--- app/login/page.js
+++ app/login/page.js
@@ -1,4 +1,21 @@
+import { createMetaData } from "@lib/metadata";
 import Login from "./login";
+
+// export async function generateMetadata({ searchParams }) {
+
+//     if (!searchParams.callback) {
+//         return createMetaData({
+            
+//         });
+//     }
+   
+//     return {
+//       title: product.title,
+//       openGraph: {
+//         images: ['/some-specific-page-image.jpg'],
+//       },
+//     }
+// }
 
 export default function Page() {
     return <Login/>
