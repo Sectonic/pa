77db9c0f76eb748497b113b91c8b3c4fb969c931
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-11-01 01:15:42 -0600

    SEO for login required

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,15 @@
 import { useUser } from "@lib/user";
 import DashboardBanner from "./banner";
+import { getSession } from "@lib/session";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
+
+    const session = getSession();
+    if (!session) {
+      redirect('/login?' + new URLSearchParams({callback: '/dashboard', error: 'An account is required for Dashboard'}));
+    }
+
     const profile = await useUser(true);
 
     return (
--- app/login/page.js
+++ app/login/page.js
@@ -1,21 +1,39 @@
 import { createMetaData } from "@lib/metadata";
 import Login from "./login";
 
-// export async function generateMetadata({ searchParams }) {
+export async function generateMetadata({ searchParams }) {
 
-//     if (!searchParams.callback) {
-//         return createMetaData({
-            
-//         });
-//     }
-   
-//     return {
-//       title: product.title,
-//       openGraph: {
-//         images: ['/some-specific-page-image.jpg'],
-//       },
-//     }
-// }
+    if (!searchParams.callback) {
+        return createMetaData({});
+    }
+
+    if (searchParams.callback.includes('typechart')) {
+      return createMetaData({
+        title: 'TypeChart',
+        description: 'Analyze types through diagrams, trait spectrums, coins, and examples.',
+        url: searchParams.callback
+      });
+    }
+
+    if (searchParams.callback.includes('typesearch')) {
+      return createMetaData({
+        title: 'TypeSearch',
+        description: 'Search through a personality database of 1900+ officially typed individuals',
+        url: searchParams.callback
+      });
+    }
+
+    if (searchParams.callback.includes('typechart')) {
+      return createMetaData({
+        title: 'Dashboard',
+        description: 'Home to your Personality Academy profile',
+        url: searchParams.callback
+      });
+    }
+
+    return createMetaData({});
+
+}
 
 export default function Page() {
     return <Login/>
