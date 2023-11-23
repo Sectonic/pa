9586719256d263c9786a3928f76fcb07ba0e9953
Sujal Dhakal <sujaldhakal06@gmail.com>
2023-11-22 17:04:38 -0700

    remove count type migration

---
+++ prisma/migrations/20231123000413_remove_count_type/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `count` on the `TypeData` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `TypeData` DROP COLUMN `count`;
