96525a6fa7813ea21bac149f345a678564642796
Sectonic <sujaldhakal06@gmail.com>
2024-05-20 18:08:21 -0600

    added cookie reset

--- app/provider.js
+++ app/provider.js
@@ -1,8 +1,22 @@
 'use client';
 
+import { cookieOptions } from '@components/config';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { deleteCookie, hasCookie, setCookie } from 'cookies-next';
+import { useEffect } from 'react';
 
 export default function Provider({ children }) {
+
+    useEffect(() => {
+
+        if (!hasCookie('5-20-24-PA-Reset') && hasCookie('PAsession')) {
+            deleteCookie('PAsession');
+            setCookie('5-20-24-PA-Reset', 'true', cookieOptions);
+            window.location.reload();
+        }   
+
+    }, [])
+
     return (
         <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_ID}`}>
             {children}
